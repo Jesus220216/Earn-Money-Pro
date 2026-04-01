7339f7c1797f66fc76810c4018eb5396a9caaab7
Jesus220216 <jjr1602@gmail.com>
2026-04-01 12:16:51 -0400

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,9 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "XXXX",
-  authDomain: "XXXX",
-  projectId: "XXXX",
+  apiKey: "AIzaSyAr9_H2sbtNM0eg7tXomHBwIhDpDAYXz1w",
+  authDomain: "earn-money-pro-a92c7.firebaseapp.com",
+  projectId: "earn-money-pro-a92c7",
+  storageBucket: "earn-money-pro-a92c7.firebasestorage.app",
+  messagingSenderId: "197560363475",
+  appId: "1:197560363475:web:672ab7cdc0fdcb8369a726",
+  measurementId: "G-F10ELC29CY"
 };
 
 const app = initializeApp(firebaseConfig);
