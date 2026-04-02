761ddc2c690941d809efa60132d7aacfcc2dfa7c
Jesus220216 <jjr1602@gmail.com>
2026-04-02 14:46:03 -0400

    index.js

--- index.js
+++ index.js
@@ -14,10 +14,15 @@ admin.initializeApp({
 const db = admin.firestore();
 
 // 📁 FRONTEND
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "admin.html"));
 });
 
 // 🔐 SECRET
