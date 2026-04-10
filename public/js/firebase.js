8fe8fea8c8a0f35c986923835a0bb3648902c8e3
Jesus220216 <jjr1602@gmail.com>
2026-04-10 09:17:53 -0400

    firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -6,14 +6,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyBklJc4WZjOsE7XroizPn73hTryR3lXwhY",
   authDomain: "earn-money-pro-626e6.firebaseapp.com",
   projectId: "earn-money-pro-626e6",
-  storageBucket: "earn-money-pro-626e6.firebasestorage.app",
+  storageBucket: "earn-money-pro-626e6.appspot.com",
   messagingSenderId: "403991623859",
-  appId: "1:403991623859:web:dfbf6a8f7f4b4562b058c6",
-  measurementId: "G-DJ3LWJEBW6"
+  appId: "1:403991623859:web:dfbf6a8f7f4b4562b058c6"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
