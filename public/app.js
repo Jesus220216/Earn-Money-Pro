eece13a21435dfbffff31c9c5a9c4150d2bf3836
Jesus220216 <jjr1602@gmail.com>
2026-04-09 21:01:45 -0400

    app.js

--- public/app.js
+++ public/app.js
@@ -1,15 +1,18 @@
-// ============================================
-// 📺 CARGAR SCRIPT DE MONETIZACIÓN (CONTROLADO)
-// ============================================
-function loadMonetizationScript() {
-  // Evitar que cargue varias veces
-  if (window.scriptLoaded) return;
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
-  const s = document.createElement("script");
-  s.src = "https://playabledownloads.com/script_include.php?id=1889113";
-  s.async = true;
+const firebaseConfig = {
+  apiKey: "AIzaSyBklJc4WZjOsE7XroizPn73hTryR3lXwhY",
+  authDomain: "earn-money-pro-626e6.firebaseapp.com",
+  projectId: "earn-money-pro-626e6",
+  storageBucket: "earn-money-pro-626e6.firebasestorage.app",
+  messagingSenderId: "403991623859",
+  appId: "1:403991623859:web:dfbf6a8f7f4b4562b058c6",
+  measurementId: "G-DJ3LWJEBW6"
+};
 
-  document.body.appendChild(s);
+const app = initializeApp(firebaseConfig);
 
-  window.scriptLoaded = true;
-}
+export const auth = getAuth(app);
+export const db = getFirestore(app);
