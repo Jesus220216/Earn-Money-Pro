b491d71d15133c96eae04e8f8e8787d021421e25
Jesus220216 <jjr1602@gmail.com>
2026-04-12 16:02:36 -0400

    index.js

--- index.js
+++ index.js
@@ -56,9 +56,7 @@ app.all("/postback", async (req, res) => {
     const payout = parseFloat(data.payout || 0);
     const password = data.password;
 
-   const password = (data.password || "").trim();
-
-if (password && password !== "Jhadenjerielpro2201") return res.send("denied");
+    if (password && password !== "Jhadenjerielpro2201") return res.send("denied");
     if (!subid || isNaN(payout)) return res.send("invalid");
 
     const userRef = db.collection("users").doc(subid);
